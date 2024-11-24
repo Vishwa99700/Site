16cdf0f2f232caf2df1e49f253502354c801ed7d
Vishwa Sudaraka <vishwa25flash@gmail.com>
2024-11-24 10:33:04 +0530

    Add files via upload

---
+++ db.sql
@@ -0,0 +1,3 @@
+create database vishwa;
+use vishwa;
+create table users(userid INT primary key auto_increment,mail varchar(255),pswd varchar(255));
---
+++ home.php
@@ -0,0 +1,10 @@
+<html>
+ <head> 
+	<title>My Page 1</title>
+
+ </head>
+ <body style="background-color:#D6EAF8;">
+ <h1 align="center" style="color:Blue;"> WELCOME</H1>
+	<img src="860.png" width="1500" height="700" align="center">
+ </body>
+</html>	
